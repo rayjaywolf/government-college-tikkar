10d4410356443e2192ca43e98097acb91d7f510d
rayjay8 <jintarehaan@gmail.com>
2024-10-20 11:04:53 +0530

    changes

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ import "./style.scss";
 let menu = document.querySelector(".menu");
 let headerMobileContent = document.querySelector(".header-mobile-content");
 
-// when menu is clicked, toggle the class "open" on the menu
 menu.addEventListener("click", () => {
   menu.classList.toggle("open");
   headerMobileContent.classList.toggle("active");
